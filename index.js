05580af55e5f98b55e3916b4927332b20fbbf4aa
BookmDan <danoh329@gmail.com>
2023-07-02 18:06:28 -0700

    changes

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+// const input = document.getElementById('button');
+
 function addingEventListener() {
+
+  const button = document.getElementById('button');
+
+  // Define the event listener function
+  function handleClick() {
+    console.log('Button clicked!');
+  }
+
+  // Bind the event listener to the button element
+  button.addEventListener('click', handleClick);
+
+
+  // alert('I was clicked!');
 }
+
+// input.addEventListener('click', clickAlert);
+
+
+-- 
+Daniel Oh
+Colorado State University '25
+Master's CS Major
+“It takes courage to be kind” - Maya Angelou
+
+
+
